3b3ba84d6e81dc2333f84157fcf290b8231caf9b
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-02-24 02:35:56 +0100

    integrate new chat to firebase

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -12,7 +12,6 @@ const NewChat = () => {
   const createNewChat = async () => {
     const doc = await addDoc(
       collection(db, "users", session?.user?.email!, "chats"), {
-      messages: [],
       userId: session?.user?.email!,
       createdAt: serverTimestamp()
     }
